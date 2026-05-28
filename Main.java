0430da664858495987e1b25e084d5915f1280a0f
amalxxo <alexllanez11@gmail.com>
2026-05-28 00:06:50 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -5,49 +5,65 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         
-       /*String[] productos = {"Mouse", "Teclado", "Monitor", "Tarj. Gráfica", "Cables", "Cargadores"};
-        double[] precios = {5.99, 8.92, 114.15, 568.91, 3.00, 22.00};
-        int[] cantidades = new int [6];
+      System.out.println("WELCOME TO BINGO!");
 
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Ingrese la cantidad de cada producto: ");
-        for (int i =0; i<precios.length; i++){
-            System.out.println("Para " + productos[i] + " con precio $ "+ precios[i]);
-            String input = scanner.nextLine();
-            if(input == "exit"){
-                System.exit(0);
-            }else {
-                cantidades[i] = Integer.parseInt(input);
+        String[][] bingo;
+        bingo = new String[5][5];
+
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < bingo.length; j++) {
+                int aux;
+                boolean repetido;
+                do {
+                    aux = (int) (Math.random() * 100);
+                    repetido = false;
+
+                    for (int f = 0; f < bingo.length; f++) {
+                        for (int c = 0; c < bingo.length; c++) {
+
+                            if (bingo[f][c] != null && bingo[f][c].equals(String.valueOf(aux))) {
+                                repetido = true;
+                            }
+                        }
+                    }
+
+                } while (repetido);
+
+                bingo[i][j] = String.valueOf(aux);
             }
         }
 
-        scanner.close();
-
-        for (int i = 0; i < productos.length; i++) {
-            System.out.println("Producto:" + productos[i] + " Precio:" + precios[i] + " Cantidad: " + cantidades[i]);
+        for (int i = 0; i < bingo.length; i++) {
+            for (int j = 0; j < bingo.length; j++) {
+                System.out.print(bingo[i][j] + "\t");
+            }
+            System.out.println();
         }
 
-        for(int i=0; i<productos.length; i++){
-            System.out.println("Ingrese el precio del producto: " + productos[i]);
-                precios_cantidades[i][0] = scanner.nextDouble();
-            
-            System.out.println("Ingrese la cantidad del producto: " + productos[i]);
-            precios_cantidades[i][1] = scanner.nextDouble();
-        }*/
+        while (true) {
 
-            int[][] bingo = new int[5][5];
-            int players = 0;
-            System.out.println("Welcome to Bingo!");
-            System.out.println("Player" + (players+1) + " Draw Number: " + (int)(Math.random() * 100));
-            for(int i=0; i<bingo.length; i++){
-                for(int j=0; j<bingo[i].length; j++){
-                    bingo[i][j] = (int)(Math.random() * 100);
-                    System.out.print(bingo[i][j]+ " ");
+            System.out.println("Ingrese fila: ");
+            int fila = sc.nextInt();
+
+            System.out.println("Ingrese columna: ");
+            int columna = sc.nextInt();
+
+            if (fila < 0 || fila >= bingo.length ||
+                    columna < 0 || columna >= bingo[0].length) {
+
+                System.out.println("Posición inválida");
+                continue;
+            }
+            bingo[fila][columna] = "X";
+
+            for (int i = 0; i < bingo.length; i++) {
+                for (int j = 0; j < bingo.length; j++) {
+                    System.out.print(bingo[i][j] + "\t");
                 }
                 System.out.println();
             }
-
-            
- }
+        }
+    }
 }
